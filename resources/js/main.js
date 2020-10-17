daedba75a684441bc588cae71585c5472e9986b5
junaet_hossain <junaetcse@gmail.com>
2020-10-18 01:51:15 +0600

    implement nav menu as fixed and add jquery pluggin waypoint for sticky fixed

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,14 @@
-$(document).ready( function(){
+$(document).ready(function() {
+
+    $('.js--service-section').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass("sticky")
+        } else {
+            $('nav').removeClass("sticky")
+        }
+    });
+
+
     var mixer = mixitup('.container');
 
 });
