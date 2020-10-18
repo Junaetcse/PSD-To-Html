94f5da1f66062ff95ba2e6d4cfc12da64f788764
junaet_hossain <junaetcse@gmail.com>
2020-10-18 18:14:27 +0600

    implement navigation menu for mobile version

--- resources/js/main.js
+++ resources/js/main.js
@@ -10,5 +10,12 @@ $(document).ready(function() {
 
 
     var mixer = mixitup('.container');
-
 });
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
